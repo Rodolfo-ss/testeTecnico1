567a39330ee5f3b5bc69eb9933ba15314abd1500
unknown <rodolfcs@gmail.com>
2023-10-29 21:24:03 -0300

    teste

---
+++ testeTecnico.rb
@@ -0,0 +1,38 @@
+#Questao 1
+
+def palindromo(word)
+    i = 0
+    if word.reverse == word
+        puts  "#{word} is a palindrome"
+    else
+        puts "#{word} is not a palindrome"
+    end
+end
+
+#puts palindromo("peep")
+
+
+#Questao 2
+
+#Teste cpf
+
+    /^\d{3}\.\d{3}\.d{3}\-\d{2}$/.test(212.244.346-33)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
