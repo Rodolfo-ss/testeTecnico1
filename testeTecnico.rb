b7ac0990c6222c1795ba6d2a93719e3683b901cd
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-10-29 21:35:00 -0300

    Update testeTecnico.rb

--- testeTecnico.rb
+++ testeTecnico.rb
@@ -1,4 +1,4 @@
-#Questao 1
+#Questao 1 - Uma verificação de um palindromo 
 
 def palindromo(word)
     i = 0
@@ -9,17 +9,20 @@ def palindromo(word)
     end
 end
 
-#puts palindromo("peep")
+puts palindromo("peep")
 
 
 #Questao 2
 
-#Teste cpf
+#Teste cpf - Um regex que valida um CPF
 
     /^\d{3}\.\d{3}\.d{3}\-\d{2}$/.test(212.244.346-33)
 
 
 #Questao 3
+
+#Implemente uma funcao que calcula o fatorial de numeros arbitrariamente grandes.
+
 
 
 
